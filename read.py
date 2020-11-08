db1be9f58c9a41757dc03558c5ff55b06113702a
hotingwei <anitaho577@gmail.com>
2020-11-08 10:10:08 +0100

    calculate avg length of reviews

--- read.py
+++ read.py
@@ -7,4 +7,9 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: # %是和後面數字求餘數的意思
 			print(len(data))
 
-print(data[0])
+print('The file has all been read. There are', len(data), 'datas in total.')
+
+sum_len = 0
+for d in data: #d 為字串
+	sum_len = sum_len + len(d)
+print('The average of the comments is', sum_len / len(data))
